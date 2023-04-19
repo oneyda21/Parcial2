31ece977d30367e73ff208ef7564cc2321cba831
oneyda21 <124007400+oneyda21@users.noreply.github.com>
2023-04-18 20:35:01 -0600

    PARCIAL

---
+++ Parcial2/Program.cs
@@ -0,0 +1,10 @@
+﻿try
+{
+    int dato1 = 16;
+    String dato2 = "25g";
+    int resultado = dato1 + Convert.ToInt32(dato2);
+}
+catch (Exception)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
